afcd09ece65edfb026eeb558f569f42996c891c0
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2022-05-12 00:31:33 -0400

    Update deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
-  :plugins [[speclj "3.3.2"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :profiles {:dev {:dependencies [[speclj "3.4.1"]]}}
+  :plugins [[speclj "3.4.1"]]
   :test-paths ["spec"])
